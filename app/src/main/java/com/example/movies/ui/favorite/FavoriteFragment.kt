3f0f74b6868e016ff250e3e9f763a8c3f127674e
ainsag <200103466@stu.sdu.edu.kz>
2022-12-12 05:53:44 +0600

    bottom nav necessaries

---
+++ app/src/main/java/com/example/movies/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,7 @@
+package com.example.movies.ui.favorite
+
+import androidx.fragment.app.Fragment
+import com.example.movies.R
+
+class FavoriteFragment:Fragment(R.layout.fragment_favorite) {
+}
---
+++ app/src/main/java/com/example/movies/ui/movie/MovieFragment.kt
@@ -0,0 +1,7 @@
+package com.example.movies.ui.movie
+
+import androidx.fragment.app.Fragment
+import com.example.movies.R
+
+class MovieFragment: Fragment(R.layout.fragment_movie) {
+}

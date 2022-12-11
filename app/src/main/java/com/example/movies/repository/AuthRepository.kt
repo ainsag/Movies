93e4cf03fca3c067cec9108bad73901911bedbd2
aeedaa <200103275@stu.sdu.edu.kz>
2022-12-12 00:11:25 +0600

    Created Repository for Authentication

--- app/src/main/java/com/example/movies/repository/AuthRepository.kt
+++ app/src/main/java/com/example/movies/repository/AuthRepository.kt
@@ -1,4 +1,40 @@
 package com.example.movies.repository
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 class AuthRepository {
+    lateinit var auth: FirebaseAuth
+
+    var firebaseSignUpUser: MutableLiveData<FirebaseUser> = MutableLiveData()
+    var firebaseSignInUser: MutableLiveData<FirebaseUser> = MutableLiveData()
+
+    fun registerUser(email:String ,password:String){
+        auth = FirebaseAuth.getInstance()
+        auth.createUserWithEmailAndPassword(email , password).addOnCompleteListener{
+            if(it.isSuccessful){
+                firebaseSignUpUser.value = auth.currentUser
+                Log.d("FbRepo" , "this is from here22 ${it.result} , ${firebaseSignUpUser}")
+            }else{
+                Log.d("FbRepo" , "this is from here ${it.result} , ${firebaseSignUpUser}")
+            }
+        }
+    }
+
+    fun loginUser(email: String , password: String){
+        auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email , password).addOnCompleteListener {
+            if (it.isSuccessful)
+            {
+                firebaseSignInUser.value = auth.currentUser
+                Log.d("FbRepo" ,  "${it.isComplete},${it.isSuccessful},${it.result},${auth.currentUser}")
+            }
+            else{
+                Log.d("FbRepo" , "${it.isComplete},${it.isSuccessful},${it.result},${auth.currentUser}")
+            }
+            Log.d("FbRepo","${it.isComplete},${it.isSuccessful},${it.result},${auth.currentUser}")
+        }
+    }
 }
